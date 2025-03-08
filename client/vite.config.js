e4b671934d8aeb09735920d8d5c127fc424a023e
Adnan-Ahmed07 <masudrana52443@gmail.com>
2025-03-08 02:14:11 -0700

    try to deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'node:path'  
+import path from 'node:path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()], 
+  plugins: [react()],
+  css: {
+    postcss: './postcss.config.mjs' // Link to your PostCSS config
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
