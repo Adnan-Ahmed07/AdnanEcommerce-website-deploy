9ec09a20a6993ecbc05876c011ef33f1ed87cc8f
Adnan-Ahmed07 <masudrana52443@gmail.com>
2025-03-07 23:00:50 -0700

    try one more deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'node:path'  // Correct usage for ES modules
+import path from 'node:path'  
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()], // Tailwind doesn't need to be imported here
+  plugins: [react()], 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
