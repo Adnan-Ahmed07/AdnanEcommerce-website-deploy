28e6a14258263f5561ebd3927a361a4a90a219ea
Adnan-Ahmed07 <masudrana52443@gmail.com>
2025-03-08 03:05:54 -0700

    have many bug try to deploy it

--- client/src/components/admin-view/layout.jsx
+++ client/src/components/admin-view/layout.jsx
@@ -1,25 +1,23 @@
 import { Outlet } from "react-router-dom";
-import AdminSideBar from "./sidebar";
-import AdminHeader from "./header.jsx";  
+import AdminSidebar from "./sidebar";
+ 
 import { useState } from "react";
-const AdminMlayout = () => {
-  const [openSidebar, setOpenSidebar] = useState(false);
-  return (
-    
-    <div className="flex min-h-screen w-full">
-  
-  <AdminSideBar open={openSidebar} setOpen={setOpenSidebar} /> 
-    <div className="flex flex-1 flex-col">
-    
-    <AdminHeader setOpen={setOpenSidebar}/>
-      <main className="flex-1 flex-col flex bg-muted/40 p-4 md:p-6">
-        <Outlet />
-      </main>
-    </div>
-  </div>
+import AdminHeader from "./AdminHeader";
 
+const AdminLayout = () => {
+    const [openSidebar, setOpenSidebar] = useState(false);
     
-  )
-}
+    return (
+        <div className="flex min-h-screen w-full">
+            <AdminSidebar open={openSidebar} setOpen={setOpenSidebar} />
+            <div className="flex flex-1 flex-col">
+                <AdminHeader setOpen={setOpenSidebar} />
+                <main className="flex-1 flex-col flex bg-muted/40 p-4 md:p-6">
+                    <Outlet />
+                </main>
+            </div>
+        </div>
+    );
+};
 
-export default AdminMlayout;
+export default AdminLayout;
